1f23a8b618d163af412598c86514f62fb7812249
Poshan M S <siddeshwaraprasanna5@gmail.com>
2026-05-10 20:01:26 +0530

    Add User model

--- models.py
+++ models.py
@@ -4,3 +4,19 @@ from datetime import datetime
 
 # Create the db object - we will connect it to Flask app later in app.py
 db = SQLAlchemy
+
+# User table - stores registered users
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # One user can have many notes
+    notes = db.relationship('Note', backref='author', lazy=True, cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<User {self.email}>'
