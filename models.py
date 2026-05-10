4d841d2a7414b8febc985ca5486cc3f91b7493b9
Poshan M S <siddeshwaraprasanna5@gmail.com>
2026-05-10 20:11:18 +0530

    Add Note model

--- models.py
+++ models.py
@@ -20,3 +20,18 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.email}>'
+    
+class Note(db.Model):
+    __tablename__ = 'notes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.Foreign('user.id'), nullable=False)
+    title = db.Column(db.String(200))
+    content = db.Column(db.Text)
+    color = db.Column(db.String(20), default='white')
+    pinned = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DataTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DataTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Note {self.id}>'
